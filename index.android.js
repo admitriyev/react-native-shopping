6afb905358d08c5db452b0d24cd5a97b1e29fff7
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2016-07-27 11:07:03 -0600

    added base container

--- index.android.js
+++ index.android.js
@@ -9,23 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableHighlight
 } from 'react-native';
+
+class BaseContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+  }
+
+  render() {
+    console.log("BaseContainer:render props " + JSON.stringify(this.props));
+    let image_uri = this.props.image
+    return (
+      <TouchableHighlight onPress={this._onPressButton.bind(this)}>
+        <Image source={{uri: image_uri}}
+          style={{width: 400, height: 400}} />
+      </TouchableHighlight>
+    );
+  }
+
+  _onPressButton() {
+    console.log("_onPressButton props" + JSON.stringify(this.props));
+  }
+
+}
 
 class ReactNativeShopping extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <BaseContainer image='https://s3-us-west-2.amazonaws.com/admitriyev-images/show-001.jpg'/>
+        <BaseContainer image='https://s3-us-west-2.amazonaws.com/admitriyev-images/product-001.jpg'/>
       </View>
     );
   }
@@ -37,17 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeShopping', () => ReactNativeShopping);
